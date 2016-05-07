3f4a59b070ce382b87c949e4b66a318896e1ef50
Ahmed AbouElhamayed <alwahsh.ahmed@gmail.com>
2016-05-07 17:49:02 +0300

    Add needed attributes

---
+++ api/models/User.js
@@ -0,0 +1,77 @@
+// Original file here:
+// https://github.com/langateam/sails-auth/blob/master/api/models/User.js
+var _ = require('lodash');
+var crypto = require('crypto');
+var randomstring = require("randomstring");
+
+/** @module User */
+module.exports = {
+  attributes: {
+    username: {
+      type: 'string',
+      unique: true,
+      index: true,
+      notNull: true
+    },
+    email: {
+      type: 'email',
+      unique: true,
+      index: true
+    },
+    first_name: {
+      type: 'string'
+    },
+    last_name: {
+      type: 'string'
+    },
+    confirmed_at: {
+      type: 'datetime'
+    },
+    confirmation_token: {
+      type: 'string'
+    },
+    passports: {
+      collection: 'Passport',
+      via: 'user'
+    },
+
+    toJSON: function () {
+      var user = this.toObject();
+      delete user.password;
+      return user;
+    },
+
+    /**
+     * Require confirmation of email
+     */
+    require_confirmation: function() {
+     this.confirmation_token = randomstring.generate(); // Should better save a hashed version.
+     this.confirmed_at = null;
+     // TODO: Send E-mail.
+     this.save(function() {});
+    }
+
+  },
+
+  afterCreate: function (attr, next) {
+    User.findOne({
+      id: attr.id
+    }).exec(function(err, user) {
+      user.require_confirmation();
+    });
+    next();
+  },
+
+  /**
+   * Register a new User with a passport
+   */
+  register: function (user) {
+    return new Promise(function (resolve, reject) {
+      sails.services.passport.protocols.local.createUser(user, function (error, created) {
+        if (error) return reject(error);
+
+        resolve(created);
+      });
+    });
+  }
+};
